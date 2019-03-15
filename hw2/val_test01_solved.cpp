7b6c4b28e837c15392f127215b6b67bdcdb8826f
Dmitriy Kunisky <tkunisky@gmail.com>
2019-03-15 18:49:30 -0400

    Add hw2 valgrind solutions

---
+++ hw2/val_test01_solved.cpp
@@ -0,0 +1,99 @@
+# include <cstdlib>
+# include <iostream>
+
+using namespace std;
+
+int main ( );
+void f ( int n );
+
+//****************************************************************************80
+
+int main ( )
+
+//****************************************************************************80
+//
+//  Purpose:
+//
+//    MAIN is the main program for TEST01.
+//
+//  Discussion:
+//
+//    TEST01 calls F, which has a memory "leak".  This memory leak can be
+//    detected by VALGRID.
+//
+//  Licensing:
+//
+//    This code is distributed under the GNU LGPL license.
+//
+//  Modified:
+//
+//    18 May 2011
+//
+{
+  int n = 10;
+
+  cout << "\n";
+  cout << "TEST01\n";
+  cout << "  C++ version.\n";
+  cout << "  A sample code for analysis by VALGRIND.\n";
+
+  f ( n );
+//
+//  Terminate.
+//
+  cout << "\n";
+  cout << "TEST01\n";
+  cout << "  Normal end of execution.\n";
+
+  return 0;
+}
+//****************************************************************************80
+
+void f ( int n )
+
+//****************************************************************************80
+//
+//  Purpose:
+//
+//    F computes N+1 entries of the Fibonacci sequence.
+//
+//  Licensing:
+//
+//    This code is distributed under the GNU LGPL license.
+//
+//  Modified:
+//
+//    18 May 2011
+//
+{
+  int i;
+  int *x;
+
+  // SOLUTION Part 1:
+  // Since x[i] is accessed in the range 0 <= i <= n in the code below, we
+  // need the size of x to be n + 1 rather than n.
+
+  // x = ( int * ) malloc ( n * sizeof ( int ) );
+  x = ( int * ) malloc ( (n + 1) * sizeof ( int ) );
+
+  x[0] = 1;
+  cout << "  " << 0 << "  " << x[0] << "\n";
+
+  x[1] = 1;
+  cout << "  " << 1 << "  " << x[1] << "\n";
+
+  for ( i = 2; i <= n; i++ )
+  {
+    x[i] = x[i-1] + x[i-2];
+    cout << "  " << i << "  " << x[i] << "\n";
+  }
+
+  // SOLUTION Part 2:
+  // Since x was allocated with `malloc` rather than `new`, it should be
+  // deallocated with `free` rather than `delete`.
+  
+  // delete [] x;
+  free(x);
+
+  return;
+}
