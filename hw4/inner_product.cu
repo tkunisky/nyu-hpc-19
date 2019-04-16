198c9252971e6a0f8a81d7eec21a8500aa5d5aef
Dmitriy Kunisky <tkunisky@gmail.com>
2019-04-15 21:56:24 -0400

    Update problem 1

--- hw4/inner_product.cu
+++ hw4/inner_product.cu
@@ -17,6 +17,40 @@ void Check_CUDA_Error(const char *message){
 
 #define BLOCK_SIZE 1024
 
+// Reduction kernel for sum
+__global__ void reduction_kernel(double* sum, const double* a, long N, long m) {
+	__shared__ double smem[BLOCK_SIZE];
+	int idx = (blockIdx.x) * blockDim.x + threadIdx.x;
+	int idx_y = blockIdx.y;
+	if (idx < N) smem[threadIdx.x] = a[m*idx + idx_y];
+	else smem[threadIdx.x] = 0;
+
+	__syncthreads();
+	if (threadIdx.x < 512) smem[threadIdx.x] += smem[threadIdx.x + 512];
+	__syncthreads();
+	if (threadIdx.x < 256) smem[threadIdx.x] += smem[threadIdx.x + 256];
+	__syncthreads();
+	if (threadIdx.x < 128) smem[threadIdx.x] += smem[threadIdx.x + 128];
+	__syncthreads();
+	if (threadIdx.x <  64) smem[threadIdx.x] += smem[threadIdx.x + 64];
+	__syncthreads();
+	if (threadIdx.x <  32) {
+		smem[threadIdx.x] += smem[threadIdx.x + 32];
+		__syncwarp();
+		smem[threadIdx.x] += smem[threadIdx.x + 16];
+		__syncwarp();
+		smem[threadIdx.x] += smem[threadIdx.x + 8];
+		__syncwarp();
+		smem[threadIdx.x] += smem[threadIdx.x + 4];
+		__syncwarp();
+		smem[threadIdx.x] += smem[threadIdx.x + 2];
+		__syncwarp();
+		if (threadIdx.x == 0) {
+			sum[m*blockIdx.x + idx_y] = smem[0] + smem[1];
+		}
+	}
+}
+
 // CPU inner product implementation
 void inner_product_ref(double* ip_ptr, const double* a, const double* b, long N) {
   double sum = 0;
@@ -24,6 +58,8 @@ void inner_product_ref(double* ip_ptr, const double* a, const double* b, long N)
   for (long i = 0; i < N; i++) sum += a[i] * b[i];
   *ip_ptr = sum;
 }
+
+
 
 // GPU inner product kernel
 __global__ void inner_product_kernel(double* ip, const double* a, const double* b, long N){
@@ -58,11 +94,22 @@ __global__ void inner_product_kernel(double* ip, const double* a, const double* 
   }
 }
 
+// Pointwise multiplication kernel
+__global__ void pointwise_mult_kernel(double* c, const double* a, const double* c, long N) {
+  int idx = (blockIdx.x) * blockDim.x + threadIdx.x;
+  if (idx < N) {
+    c[idx] = a[idx] * b[idx];
+  }
+}
+
 // Wrapper for inner product kernel
 void inner_product(double* ip, double* x, double* y, long N) {
-  double *x_d, *y_d, *z_d;
+  double *x_d, *y_d, *xy_d, *z_d;
   cudaMalloc(&x_d, N*sizeof(double));
   cudaMalloc(&y_d, N*sizeof(double));
+
+  cudaMemcpyAsync(x_d, x, N*sizeof(double), cudaMemcpyHostToDevice);
+  cudaMemcpyAsync(y_d, y, N*sizeof(double), cudaMemcpyHostToDevice);
 
   // Extra memory buffer for reduction across thread-blocks
   long N_work = 1;
@@ -73,18 +120,18 @@ void inner_product(double* ip, double* x, double* y, long N) {
   }
   cudaMalloc(&z_d, N_work*sizeof(double));
 
-  cudaMemcpyAsync(x_d, x, N*sizeof(double), cudaMemcpyHostToDevice);
-  cudaMemcpyAsync(y_d, y, N*sizeof(double), cudaMemcpyHostToDevice);
   // cudaMemcpyAsync(z_d, y, N*sizeof(double), cudaMemcpyHostToDevice);
   cudaDeviceSynchronize();
 
+  pointwise_mult_kernel<<<N/BLOCK_SIZE+1,BLOCK_SIZE>>>(xy_d, x_d, y_d, N);
+
   double* ip_d = z_d;
   long Nb = (N + BLOCK_SIZE - 1) / (BLOCK_SIZE);
-  inner_product_kernel<<<Nb,BLOCK_SIZE>>>(ip_d, x_d, y_d, N);
+  reduction_kernel<<<Nb,BLOCK_SIZE>>>(z_d, xy_d, N);
   while (Nb > 1) {
     long this_N = Nb;
     Nb = (Nb + BLOCK_SIZE - 1) / (BLOCK_SIZE);
-    inner_product_kernel<<<Nb,BLOCK_SIZE>>>(ip_d + Nb, ip_d, this_N);
+    inner_product_kernel<<<Nb,BLOCK_SIZE>>>(z_d + Nb, z_d, this_N);
   }
 
   cudaMemcpyAsync(ip, z_d, 1*sizeof(double), cudaMemcpyDeviceToHost);
