75259e9d40ef32088058a8b4e17ac73d49f69cf7
Dmitriy Kunisky <Tim Kunisky>
2019-05-14 02:10:45 -0400

    Update Jacobi sbatch

--- hw6/jacobi2D-mpi.s
+++ hw6/jacobi2D-mpi.s
@@ -3,12 +3,8 @@
 module purge
 module load openmpi/gnu/2.0.2
 
-cd $SCRATCH/nyu-hpc-19/hw6
-rm jacobi2D-mpi
-make
-
 RUNDIR=$SCRATCH/nyu-hpc-19/hw6/jacobi-run-${SLURM_JOB_ID/.*}
 mkdir -p $RUNDIR
 cd $RUNDIR
 
-mpirun $SCRATCH/nyu-hpc-19/hw6/jacobi2D-mpi $N 1000
+mpirun $SCRATCH/nyu-hpc-19/hw6/jacobi2D-mpi 2000 2000
